a6be2b845aa8441333aaf78f4090ea352e473f2e
moisemb <papemoussa.mbengue.11@gmail.com>
2025-03-23 11:23:58 +0000

    Enhance Next.js configuration by adding JSON file handling in webpack, enabling reactStrictMode, and maintaining ESLint ignore settings during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  }
+  webpack: (config, { isServer }) => {
+    // Configuration pour traiter les fichiers JSON
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'javascript/auto',
+      resolve: { fullySpecified: false }
+    });
+
+    return config;
+  },
+  // Ajouter d'autres configurations Next.js si nécessaire
+  reactStrictMode: true,
+  // Ignorer les avertissements ESLint pendant la construction (optionnel)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
